183bb201ca847825e6bb8f4078b300863096fa0a
Mickael Souza <mickael.souza.if@gmail.com>
2018-09-03 01:35:17 -0300

    Adição em posição específica

--- classes/SingleLinkedList.py
+++ classes/SingleLinkedList.py
@@ -1,7 +1,8 @@
-from Node import *
+from classes.Node import Node
 
 class SinglyLinkedList:
-   
+    __head: Node
+
     def __init__(self):
         self.__head = None
         self.__tail = None
@@ -28,9 +29,11 @@ class SinglyLinkedList:
         if self.__head is None:
             self.__head = node
             self.__tail = node
+            self.__size += 1
         else:
             self.__tail.set_next_element(node)
             self.__tail = node
+            self.__size += 1
 
     def remove_head(self):
         'Remove o elemento no inicio da lista'
@@ -39,6 +42,7 @@ class SinglyLinkedList:
             raise Exception("Empty.")
 
         self.__head = self.__head.get_next_element()
+        self.__size -= 1
 
 
     def get_head(self):
@@ -50,42 +54,40 @@ class SinglyLinkedList:
         'Retorna o elemento do final da lista'
         return self.__tail
 
-
     def print(self):
         'Imprime a lista'
         node = self.__head
+        count = 0
 
         while node is not None:
-            print(node.get_value())
+            print('Posição: '+str(count)+' Valor:'+str(node.get_value()))
             node = node.get_next_element()
+            count+=1
 
     def add_new_value(self, value, position):
         'Adicione um elemento em um lugar determinado da lista. Valor a ser inserido informado no parâmetro value. Posição informada no parâmetro position'
-        if self.__head is None:
-            raise Exception("Empty.")
-        elif value < self.__size:
-            raise Exception("Posição Inválida!")
+        if self.__size != 0 and self.__size <= position or position < 0:
+            raise Exception("Position out of range.")
+        elif self.__head is None:
+            self.__head = Node(value, None)
         else:
             count = 0
             node = self.__head
-    
+
             while node is not None:
-                previous = node.get_value()
-                node = node.get_next_element()
+                previous = node
+                next = node.get_next_element()
                 if position == count:
                     node.set_value(value)
-                    new = node.get_value
-                    previous.set_next_value(new)
-                    node.set_next_element(node)
+                    new = node
+                    previous.set_next_element(new)
+                    node.set_next_element(next)
+                    break
                 else:
+                    node = node.get_next_element()
                     count += 1
-        pass
-    
-s = SinglyLinkedList()
-s.add_head(125)
-s.add_head(200)
-s.add_head(25)
-s.add_tail(600)
-s.add_tail(700)
-s.add_new_value(5, 2)
-s.print()
+
+
+pass
+
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from classes.SingleLinkedList import *
+from classes.SingleLinkedList import SinglyLinkedList
+
+s = SinglyLinkedList()
+s.add_head(125)
+s.add_head(200)
+s.add_head(25)
+s.add_tail(600)
+s.add_tail(700)
+s.add_new_value(534, 3)
+
+s.print()
+
+
+
